c87efeb10cd7ee4def3eb9d2e301e21820363328
Rui Magalhães <m.rui.s.magalhaes@gmail.com>
2019-02-25 16:48:21 +0000

    Models User, booking and toilets done with the right dependence between them. schema done

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -1,3 +1,5 @@
 class Toilet < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_152829) do
+ActiveRecord::Schema.define(version: 2019_02_25_154508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "toilet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["toilet_id"], name: "index_bookings_on_toilet_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "toilets", force: :cascade do |t|
     t.string "name"
@@ -40,5 +49,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_152829) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "toilets"
+  add_foreign_key "bookings", "users"
   add_foreign_key "toilets", "users"
 end
