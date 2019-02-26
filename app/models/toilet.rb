aafd720fc9013f30a715d9dee011abf6aafd0548
Emanuel Moniz <emanuelandremoniz@gmail.com>
2019-02-26 00:17:49 +0000

    build index page

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -3,4 +3,5 @@ class Toilet < ApplicationRecord
   has_many :bookings
 
   mount_uploader :photo, PhotoUploader
+  validates :price, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_182310) do
+ActiveRecord::Schema.define(version: 2019_02_25_233423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_182310) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.integer "price", default: 0
     t.index ["user_id"], name: "index_toilets_on_user_id"
   end
 
