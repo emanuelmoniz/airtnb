cc3e1a100c014b1a5cce05bd34c0baee23f1733b
Emanuel Moniz <emanuelandremoniz@gmail.com>
2019-02-27 16:10:46 +0000

    add mytoilets and used toilets

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,4 +2,12 @@ class UserPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  def my_toilets?
+    true
+  end
+
+  def used_toilets?
+    true
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get 'profile', :to => 'profile#show'
+  # resources :profile, only: [:show] do
 
+  # end
+
+  get 'profile', :to => 'profile#show'
+  get 'profile/my_toilets', to: 'profile#my_toilets'
+  get 'profile/used_toilets', to: 'profile#used_toilets'
 
   root to: 'toilets#index'
   resources :toilets do
