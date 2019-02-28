4d09d30242296c14c543c7cf0156faffc234ca71
Rui Magalhães <m.rui.s.magalhaes@gmail.com>
2019-02-28 13:20:12 +0000

    eddit action of users corrected. Logo navbar with avatar changing corrected too

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :photo])
   end
 
   private
