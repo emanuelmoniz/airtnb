fb9f41273b43d5285f293d0868361d05689211a6
Rui Magalhães <m.rui.s.magalhaes@gmail.com>
2019-03-01 16:17:30 +0000

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,47 +1,32 @@
-require 'faker'
-
 Review.destroy_all
 Booking.destroy_all
 User.destroy_all
 Toilet.destroy_all
 
-
-users = []
-toilets = []
-bookings = []
-toilet_placeholder_url = "https://res.cloudinary.com/bubs/image/upload/v1551224698/toilet_placeholder.png"
-avatar_placeholder_url = "https://res.cloudinary.com/bubs/image/upload/v1551224698/avatar_placeholder.png"
-i = 0
-#create users
-6.times do
-  user = User.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name , email: "email#{i}@gmail.com", password: "123456")
-  user.remote_photo_url = avatar_placeholder_url
-  user.save
-  users << user
-  i += 1
-  puts user
-end
-
-i = 0
-#some users own toilets
-3.times do
-  toilet = Toilet.new(name: Faker::Lorem.word, description: Faker::Lorem.sentence(3), price: 15, address: Faker::Address.full_address, available: true)
-  toilet.user = users[i]
-  toilet.remote_photo_url = toilet_placeholder_url
-  toilet.save
-  toilets << toilet
-  i += 1
-  puts toilet
-end
-
-toilets = Toilet.all
-i = 0
-#some users book toilets
+user = User.new(first_name: "Rui", last_name: "Magalhães" , email: "rui.magalhaes@gmail.com", password: "123456")
+user.remote_photo_url = "https://avatars2.githubusercontent.com/u/45366558?v=4"
+user.save
 
 
-# #some users add reviewa
-# 4.times do
-#   review = Review.new(content: Faker::Vehicle.standard_specs)
-#   review.booking = bookings.sample
-#   review.save
-# end
+user = User.new(first_name: "João", last_name: "Costa" , email: "joao.costa@gmail.com", password: "123456")
+user.remote_photo_url = "https://avatars0.githubusercontent.com/u/46388982?v=4"
+user.save
+
+
+user = User.new(first_name: "Emanuel", last_name: "Moniz" , email: "emanuel.moniz@gmail.com", password: "123456")
+user.remote_photo_url = "https://avatars3.githubusercontent.com/u/10147715?v=4"
+user.save
+
+
+user = User.new(first_name: "Romain", last_name: "Aubert" , email: "romain.aubert@gmail.com", password: "123456")
+user.remote_photo_url = "https://avatars2.githubusercontent.com/u/46404814?v=4"
+user.save
+
+
+toilet = Toilet.new(name: "Sanita", description: "Fastástica", price: 2, address: "Rua do conde redondo 91b", available: true)
+toilet.user = User.all[0]
+toilet.remote_photo_url = "https://images.unsplash.com/photo-1517414628894-83d47b22f233?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"
+toilet.save
+
+
+
