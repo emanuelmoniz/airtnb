77f8556aa9885c5301efa8e93ed94a4ef9af17d4
Emanuel Moniz <emanuelandremoniz@gmail.com>
2019-03-01 18:20:54 +0000

    bug fix

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -2,10 +2,9 @@ class Toilet < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
-  validates :name, :description, :address, presence: true
+  validates :name, :description, :address, :price, :photo, presence: true
 
   mount_uploader :photo, PhotoUploader
-  validates :price, presence: true
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
